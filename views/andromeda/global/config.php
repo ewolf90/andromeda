1d7415927db0e50d2b3bf7bbb8d17011fbe5f826
Emily Wolf <12221876+ewolf90@users.noreply.github.com>
2020-07-09 22:55:43 -0400

    Update config.php

--- views/andromeda/global/config.php
+++ views/andromeda/global/config.php
@@ -47,7 +47,6 @@
 */
 
 $theme_settings = array(
-    
     // Dark Mode
     'dark_mode' => false,
     'dark_mode_button' => true,
@@ -71,7 +70,5 @@ $theme_settings = array(
     'license_link' => 'http://creativecommons.org/licenses/by-nc-sa/4.0',
     'license_image' => 'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png',
     'license_styles' => 'border-width:0'
-    
 );
-
 ?>
