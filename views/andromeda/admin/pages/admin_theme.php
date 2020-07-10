6286a67e3e23a2b1f768d3e7e67d6ddf6b2f1edf
Emily Wolf <12221876+ewolf90@users.noreply.github.com>
2020-07-09 22:55:21 -0400

    Update admin_theme.php

--- views/andromeda/admin/pages/admin_theme.php
+++ views/andromeda/admin/pages/admin_theme.php
@@ -64,8 +64,55 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
         
         // Write to the new config file
         $settings = "<?php
-    \$theme_settings = array(
-        // Dark Mode
+/*
+|---------------------------------------------------------------
+| CONFIGURATION FILE
+|---------------------------------------------------------------
+|
+| Andromeda Theme by Emily Wolf
+|
+| File: application/views/andromeda/global/config.php
+| Version: 1.0
+| GitHub: github.com/ewolf90/andromeda
+|
+| Copyright (c) 2020, Emily Wolf All rights reserved.
+| 
+| Redistribution and use in source and binary forms, with or 
+| without modification, are permitted provided that the following 
+| conditions are met:
+| 
+|   1. Redistributions of source code must retain the above 
+|      copyright notice, this list of conditions and the 
+|      following disclaimer.
+| 
+|   2. Redistributions in binary form must reproduce the above 
+|      copyright notice, this list of conditions and the 
+|      following disclaimer in the documentation and/or other 
+|      materials provided with the distribution.
+| 
+|   3. Neither the name of the copyright holder nor the names 
+|      of its contributors may be used to endorse or promote 
+|      products derived from this software without specific 
+|      prior written permission.
+| 
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+| CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+| INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+| MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+| DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+| SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+| NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+| LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+| HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,WHETHER IN 
+| CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+| OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+| EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+| 
+*/
+
+\$theme_settings = array(
+	// Dark Mode
         'dark_mode' => ".$_POST['darkMode'].",
         'dark_mode_button' => ".$_POST['darkModeButton'].",
         
@@ -88,7 +135,7 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
         'license_link' => '".$_POST['licenceLink']."',
         'license_image' => '".$_POST['licenceImage']."',
         'license_styles' => '".$_POST['licenceImageStyle']."'
-    );
+);
 ?>";
         fwrite($the_file, $settings);
         
