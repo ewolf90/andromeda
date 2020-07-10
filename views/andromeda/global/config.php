db7b59dd5ec047001fe16aca0660d8154fc27c9e
Emily Wolf <12221876+ewolf90@users.noreply.github.com>
2020-07-09 22:44:56 -0400

    Update config.php

--- views/andromeda/global/config.php
+++ views/andromeda/global/config.php
@@ -1,27 +1,77 @@
 <?php
-    $theme_settings = array(
-        // Dark Mode
-        'dark_mode' => false,
-        'dark_mode_button' => true,
+/*
+|---------------------------------------------------------------
+| THEME CONFIG FILE
+|---------------------------------------------------------------
+|
+| Andromeda Theme by Emily Wolf
+|
+| File: application/views/andromeda/global/config.php
+| Version: 1.0
+| GitHub: github.com/ewolf90/andromeda
+|
+| Copyright (c) 2020, Emily Wolf All rights reserved.
+| 
+| Redistribution and use in source and binary forms, with or 
+| without modification, are permitted provided that the following 
+| conditions are met:
+| 
+|   1. Redistributions of source code must retain the above 
+|      copyright notice, this list of conditions and the 
+|      following disclaimer.
+| 
+|   2. Redistributions in binary form must reproduce the above 
+|      copyright notice, this list of conditions and the 
+|      following disclaimer in the documentation and/or other 
+|      materials provided with the distribution.
+| 
+|   3. Neither the name of the copyright holder nor the names 
+|      of its contributors may be used to endorse or promote 
+|      products derived from this software without specific 
+|      prior written permission.
+| 
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+| CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+| INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+| MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+| DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+| SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+| NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+| LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+| HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,WHETHER IN 
+| CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+| OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+| EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+| 
+*/
+
+$theme_settings = array(
+    
+    // Dark Mode
+    'dark_mode' => false,
+    'dark_mode_button' => true,
+    
+    // Discord Widget
+    'discord_show' => false,
+    'discord_server_id' => 'SERVER_ID',
+    'discord_theme' => 'dark',
+    'discord_height' => '350px',
+    'discord_width' => '90%',
+    'discord_styles' => '',
         
-        // Discord Widget
-        'discord_show' => false,
-        'discord_server_id' => 'SERVER_ID',
-        'discord_theme' => 'dark',
-        'discord_height' => '350px',
-        'discord_width' => '90%',
-        'discord_styles' => '',
+    // Game Rating
+    'rating_show' => true,
+    'rating_language' => '1',
+    'rating_sex' => '1',
+    'rating_violence' => '1',
         
-        // Game Rating
-        'rating_show' => true,
-        'rating_language' => '1',
-        'rating_sex' => '1',
-        'rating_violence' => '1',
-        
-        // Game License
-        'license_show' => true,
-        'license_link' => 'http://creativecommons.org/licenses/by-nc-sa/4.0',
-        'license_image' => 'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png',
-        'license_styles' => 'border-width:0'
-    );
+    // Game License
+    'license_show' => true,
+    'license_link' => 'http://creativecommons.org/licenses/by-nc-sa/4.0',
+    'license_image' => 'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png',
+    'license_styles' => 'border-width:0'
+    
+);
+
 ?>
